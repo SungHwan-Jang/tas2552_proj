42b65bfdb0c9c597408cd91170be07403e7512d7
SungHwan-Jang <redmk1025@gmail.com>
2022-01-21 17:27:25 +0900

    initialize git
    update git ignore and initialize project

---
+++ MDK-ARM/RTE/_tas2552_proj/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'tas2552_proj' 
+ * Target:  'tas2552_proj' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f0xx.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
